820a49b6b44c964fdbb168236db6cd8e44871a01
Edgar Hernandez <iedgarus@gmail.com>
2021-02-02 11:59:20 -0600

    Actualizar métodos en el programa

--- Program.cs
+++ Program.cs
@@ -4,6 +4,40 @@ namespace proyecto3
 {
     class Program
     {
+        static void MostrarMenu() {
+            Console.WriteLine("* Menú *");
+            Console.WriteLine("1. Agregar libro a la biblioteca");
+            Console.WriteLine("2. Mostrar biblioteca");
+            Console.WriteLine("3. Borar libro");
+            Console.WriteLine("4. ¿Número de libros?");
+            Console.WriteLine("5. Salir");
+            Console.Write("Opción: ");
+        }
+
+        static Biblioteca AgregarLibroALaBiblioteca(Biblioteca biblioteca) {
+            string titulo, isbn, autornombre, autorapellidos;
+            Console.Write("Ingrese el título del libro: ");
+            titulo = Console.ReadLine();
+            Console.Write("Ingrese el ISBN del libro: ");
+            isbn = Console.ReadLine();
+            Console.Write("Ingrese el nombre del autor del libro: ");
+            autornombre = Console.ReadLine();
+            Console.Write("Ingrese los apellidos del autor del libro: ");
+            autorapellidos = Console.ReadLine();
+
+            Autor autor = new Autor(autornombre, autorapellidos);
+            Libro libro = new Libro(titulo, isbn);
+            libro.AgregarAutor(autor);
+            biblioteca.AgregarLibro(libro);
+            return biblioteca;
+        }
+
+        static Biblioteca BorrarLibroDeLaBiblioteca(Biblioteca biblioteca) {
+            Console.Write("Ingrese el título del libro a borrar: ");
+            biblioteca.BorrarLibro(Console.ReadLine());
+            return biblioteca;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
